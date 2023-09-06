111d81f86ac06d9d42bae6bd31ff4b082f47f31d
Jonah K. Menson <125918196+JKMenson@users.noreply.github.com>
2023-09-06 01:46:00 -0700

    first commit

---
+++ front/php/templates/header.php
@@ -0,0 +1,194 @@
+<!-- ---------------------------------------------------------------------------
+#  Pi.Alert
+#  Open Source Network Guard / WIFI & LAN intrusion detector 
+#
+#  header.php - Front module. Common header to all the web pages 
+#-------------------------------------------------------------------------------
+#  Puche 2021        pi.alert.application@gmail.com        GNU GPLv3
+#--------------------------------------------------------------------------- -->
+
+<!DOCTYPE html> 
+<html>
+
+<!-- ----------------------------------------------------------------------- -->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Pi.Alert</title>
+
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="lib/AdminLTE/bower_components/bootstrap/dist/css/bootstrap.min.css">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="lib/AdminLTE/bower_components/font-awesome/css/font-awesome.min.css">
+
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="lib/AdminLTE/bower_components/Ionicons/css/ionicons.min.css">
+
+  <!-- Theme style -->
+  <link rel="stylesheet" href="lib/AdminLTE/dist/css/AdminLTE.min.css">
+
+  <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
+        page. However, you can choose any other skin. Make sure you
+        apply the skin class to the body tag so the changes take effect. -->
+  <link rel="stylesheet" href="lib/AdminLTE/dist/css/skins/skin-yellow-light.min.css">
+
+  <!-- Pi.Alert CSS -->
+  <link rel="stylesheet" href="css/pialert.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+  <!-- Page Icon -->
+  <link rel="icon" type="image/png" sizes="160x160" href="img/pialertLogoGray80.png" />
+</head>
+
+<!-- ----------------------------------------------------------------------- -->
+<!-- Layout Boxed Yellow -->
+<body class="hold-transition skin-yellow-light layout-boxed sidebar-mini" style="background-image: url('img/background.png');">
+<!-- Site wrapper -->
+<div class="wrapper">
+
+  <!-- Main Header -->
+  <header class="main-header">
+
+<!-- ----------------------------------------------------------------------- -->
+    <!-- Logo -->
+    <a href="." class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini">P<b>a</b></span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg">Pi<b>.Alert</b></span>
+    </a>
+
+<!-- ----------------------------------------------------------------------- -->
+    <!-- Header Navbar -->
+    <nav class="navbar navbar-static-top" role="navigation">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+      <!-- Navbar Right Menu -->
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+
+          <!-- Server Name -->
+          <li><a style="pointer-events:none;"><?php echo gethostname(); ?></a></li>
+
+          <!-- Header right info -->
+          <li class="dropdown user user-menu">
+            <!-- Menu Toggle Button -->
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <!-- The user image in the navbar-->
+              <img src="img/pialertLogoWhite.png" class="user-image" style="border-radius: initial" alt="Pi.Alert Logo">
+              <!-- hidden-xs hides the username on small devices so only the image appears. -->
+              <span class="hidden-xs">Pi.Alert</span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- The user image in the menu -->
+              <li class="user-header">
+                <img src="img/pialertLogoWhite.png" class="img-circle" alt="Pi.Alert Logo" style="border-color:transparent">
+                <p>
+                  Open Source Network Guard
+                  <small>Designed for Raspberry Pi</small>
+                </p>
+              </li>
+
+              <!-- Menu Body -->
+              <li class="user-body">
+                <div class="row">
+                  <div class="col-xs-4 text-center">
+                    <a target="_blank" href="https://github.com/pucherot/Pi.Alert">GitHub Pi.Alert</a>
+                  </div>
+                  <div class="col-xs-4 text-center">
+                    <a href="mailto:pi.alert.application@gmail.com">email Support</a>
+                  </div>
+                  <div class="col-xs-4 text-center">
+                    <a target="_blank" href="https://github.com/pucherot/Pi.Alert/blob/main/LICENSE.txt">GNU GPLv3</a>
+                  </div>
+                  <!--
+                  <div class="col-xs-4 text-center">
+                    <a href="#">Updates</a>
+                  </div>
+                  -->
+                </div>
+                <!-- /.row -->
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+<!-- ----------------------------------------------------------------------- -->
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel">
+        <a href="." class="logo">
+          <img src="img/pialertLogoGray80.png" class="img-responsive" alt="Pi.Alert Logo"/>
+        </a>
+      </div>
+
+      <!-- search form (Optional) -->
+        <!-- DELETED -->
+
+      <!-- Sidebar Menu -->
+      <ul class="sidebar-menu" data-widget="tree">
+<!--
+        <li class="header">MAIN MENU</li>
+-->
+
+        <li class=" <?php if (in_array (basename($_SERVER['SCRIPT_NAME']), array('devices.php', 'deviceDetails.php') ) ){ echo 'active'; } ?>">
+          <a href="devices.php"><i class="fa fa-laptop"></i> <span>Devices</span></a>
+        </li>
+        
+<!--
+         <li><a href="devices.php?status=favorites"><i class="fa fa-star"></i> <span>Favorites Devices</span></a></li>
+-->
+        <li class=" <?php if (in_array (basename($_SERVER['SCRIPT_NAME']), array('presence.php') ) ){ echo 'active'; } ?>">
+          <a href="presence.php"><i class="fa fa-calendar"></i> <span>Presence</span></a>
+        </li>
+
+        <li class=" <?php if (in_array (basename($_SERVER['SCRIPT_NAME']), array('events.php') ) ){ echo 'active'; } ?>">
+          <a href="events.php"><i class="fa fa-bolt"></i> <span>Events</span></a>
+        </li>
+
+<!--
+        <li class="treeview">
+          <a href="#"><i class="fa fa-link"></i> <span>Config</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+          </a>
+
+          <ul class="treeview-menu">
+            <li class=" <?php if (in_array (basename($_SERVER['SCRIPT_NAME']), array('scancycles.php', 'scancyclesDetails.php') ) ){ echo 'active'; } ?>">
+              <a href="scancycles.php"><i class="fa fa-link"></i> <span>Scan Cycles</span></a>
+            </li>
+            <li><a href="#">Cron Status</a></li>
+            <li><a href="#">Current IP</a></li>
+          </ul>
+        </li>
+-->
+      </ul>
+
+      <!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+  </aside>
---
+++ front/presence.php
@@ -0,0 +1,339 @@
+<!-- ---------------------------------------------------------------------------
+#  Pi.Alert
+#  Open Source Network Guard / WIFI & LAN intrusion detector 
+#
+#  presence.php - Front module. Device Presence calendar page
+#-------------------------------------------------------------------------------
+#  Puche 2021        pi.alert.application@gmail.com        GNU GPLv3
+#--------------------------------------------------------------------------- -->
+
+<?php
+  require 'php/templates/header.php';
+?>
+
+<!-- Page ------------------------------------------------------------------ -->
+  <div class="content-wrapper">
+
+<!-- Content header--------------------------------------------------------- -->
+    <section class="content-header">
+      <h1 id="pageTitle">
+         Presence by Device
+      </h1>
+    </section>
+
+<!-- Main content ---------------------------------------------------------- -->
+    <section class="content">
+
+<!-- top small box 1 ------------------------------------------------------- -->
+      <div class="row">
+
+        <div class="col-lg-2 col-sm-4 col-xs-6">
+          <a href="#" onclick="javascript: getDevicesPresence('all');">
+            <div class="small-box bg-aqua pa-small-box-aqua pa-small-box-2">
+              <div class="inner"> <h3 id="devicesAll"> -- </h3> </div>
+              <div class="icon"> <i class="fa fa-laptop text-aqua-20"></i> </div>
+              <div class="small-box-footer pa-small-box-footer"> All Devices <i class="fa fa-arrow-circle-right"></i> </div>
+            </div>
+          </a>
+        </div>
+
+<!-- top small box 2 ------------------------------------------------------- -->
+        <div class="col-lg-2 col-sm-4 col-xs-6">
+          <a href="#" onclick="javascript: getDevicesPresence('connected');">
+            <div class="small-box bg-green pa-small-box-green pa-small-box-2">
+              <div class="inner"> <h3 id="devicesConnected"> -- </h3> </div>
+              <div class="icon"> <i class="fa fa-plug text-green-20"></i> </div>
+              <div class="small-box-footer pa-small-box-footer"> Connected <i class="fa fa-arrow-circle-right"></i> </div>
+            </div>
+          </a>
+        </div>
+
+<!-- top small box 3 ------------------------------------------------------- -->
+        <div class="col-lg-2 col-sm-4 col-xs-6">
+          <a href="#" onclick="javascript: getDevicesPresence('favorites');">
+            <div  class="small-box bg-yellow pa-small-box-yellow pa-small-box-2">
+              <div class="inner"> <h3 id="devicesFavorites"> -- </h3> </div>
+              <div class="icon"> <i class="fa fa-star text-yellow-20"></i> </div>
+              <div class="small-box-footer pa-small-box-footer"> Favorites <i class="fa fa-arrow-circle-right"></i> </div>
+            </div>
+          </a>
+        </div>
+
+<!-- top small box 4 ------------------------------------------------------- -->
+        <div class="col-lg-2 col-sm-4 col-xs-6">
+          <a href="#" onclick="javascript: getDevicesPresence('new');">
+            <div  class="small-box bg-yellow pa-small-box-yellow pa-small-box-2">
+              <div class="inner"> <h3 id="devicesNew"> -- </h3> </div>
+              <div class="icon"> <i class="ion ion-plus-round text-yellow-20"></i> </div>
+              <div class="small-box-footer pa-small-box-footer"> New Devices <i class="fa fa-arrow-circle-right"></i> </div>
+            </div>
+          </a>
+        </div>
+
+<!-- top small box 5 ------------------------------------------------------- -->
+        <div class="col-lg-2 col-sm-4 col-xs-6">
+          <a href="#" onclick="javascript: getDevicesPresence('down');">
+            <div  class="small-box bg-red pa-small-box-red pa-small-box-2">
+              <div class="inner"> <h3 id="devicesDown"> -- </h3> </div>
+              <div class="icon"> <i class="fa fa-warning text-red-20"></i> </div>
+              <div class="small-box-footer pa-small-box-footer"> Down Alerts <i class="fa fa-arrow-circle-right"></i> </div>
+            </div>
+          </a>
+        </div>
+
+<!-- top small box 6 ------------------------------------------------------- -->
+        <div class="col-lg-2 col-sm-4 col-xs-6">
+          <a href="#" onclick="javascript: getDevicesPresence('archived');">
+            <div  class="small-box bg-gray pa-small-box-gray pa-small-box-2">
+              <div class="inner"> <h3 id="devicesHidden"> -- </h3> </div>
+              <div class="icon"> <i class="fa fa-eye-slash text-gray-20"></i> </div>
+              <div class="small-box-footer pa-small-box-footer"> Hidden <i class="fa fa-arrow-circle-right"></i> </div>
+            </div>
+          </a>
+        </div>
+
+      </div>
+      <!-- /.row -->
+
+<!-- Calendar -------------------------------------------------------------- -->
+      <div class="row">
+        <div class="col-lg-12 col-sm-12 col-xs-12">
+          <div id="tableDevicesBox" class="box" style="min-height: 500px">
+
+            <!-- box-header -->
+            <div class="box-header">
+              <h3 id="tableDevicesTitle" class="box-title text-gray">Devices</h3>
+            </div>
+
+            <!-- box-body -->
+            <div class="box-body table-responsive">
+
+              <!-- spinner -->
+              <div id="loading" style="display: none">
+                <div class="pa_semitransparent-panel"></div>
+                <div class="panel panel-default pa_spinner">
+                  <table>
+                    <td width="130px" align="middle">Loading...</td>
+                    <td><i class="ion ion-ios-loop-strong fa-spin fa-2x fa-fw"></td>
+                  </table>
+                </div>
+              </div>
+
+              <!-- Calendar -->
+              <div id="calendar"></div>
+            </div>
+
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+
+<!-- ----------------------------------------------------------------------- -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+
+<!-- ----------------------------------------------------------------------- -->
+<?php
+  require 'php/templates/footer.php';
+?>
+
+
+<!-- ----------------------------------------------------------------------- -->
+<!-- fullCalendar -->
+  <link rel="stylesheet" href="lib/AdminLTE/bower_components/fullcalendar/dist/fullcalendar.min.css">
+  <link rel="stylesheet" href="lib/AdminLTE/bower_components/fullcalendar/dist/fullcalendar.print.min.css" media="print">
+  <script src="lib/AdminLTE/bower_components/moment/moment.js"></script>
+  <script src="lib/AdminLTE/bower_components/fullcalendar/dist/fullcalendar.min.js"></script>
+
+<!-- fullCalendar Scheduler -->
+  <link href="lib/fullcalendar-scheduler/scheduler.min.css" rel="stylesheet">
+  <script src="lib/fullcalendar-scheduler/scheduler.min.js"></script>  
+
+<!-- page script ----------------------------------------------------------- -->
+<script>
+
+  var deviceStatus = 'all';
+
+  // Read parameters & Initialize components
+  main();
+
+
+// -----------------------------------------------------------------------------
+function main () {
+  // Initialize components
+  $(function () {
+    initializeCalendar();
+    getDevicesTotals();
+    getDevicesPresence(deviceStatus);
+  });
+
+  // Force re-render calendar on tab change (bugfix for render error at left panel)
+  $(document).on('shown.bs.tab', 'a[data-toggle="tab"]', function (nav) {
+    if ($(nav.target).attr('href') == '#panPresence') {
+      $('#calendar').fullCalendar('rerenderEvents');
+    }
+  });
+}
+
+
+// -----------------------------------------------------------------------------
+function initializeCalendar () {
+  $('#calendar').fullCalendar({
+    header: {
+      left            : 'prev,next today',
+      center          : 'title',
+      right           : 'timelineYear,timelineMonth,timelineWeek'
+    },
+    
+    defaultView       : 'timelineMonth',
+    height            : 'auto',
+    firstDay          : 1,
+    allDaySlot        : false,
+    timeFormat        : 'H:mm', 
+
+    resourceLabelText : 'Devices',
+    resourceAreaWidth : '160px',
+    slotWidth         : '1px',
+
+    resourceOrder     : '-favorite,title',
+
+    //schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
+    schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
+
+    views: {
+      timelineYear: {
+        type              : 'timeline',
+        duration          : { year: 1 },
+        buttonText        : 'year',
+        slotLabelFormat   : 'MMM',
+        // Hack to show partial day events not as fullday events
+        slotDuration      : {minutes: 44641}
+      },
+
+      timelineQuarter: {
+        type              : 'timeline',
+        duration          : { month: 3 },
+        buttonText        : 'quarter',
+        slotLabelFormat   : 'MMM',
+        // Hack to show partial day events not as fullday events
+        slotDuration      : {minutes: 44641}
+      },
+
+      timelineMonth: {
+        type              : 'timeline',
+        duration          : { month: 1 },
+        buttonText        : 'month',
+        slotLabelFormat   : 'D',
+        // Hack to show partial day events not as fullday events
+        slotDuration      : '24:00:01'
+      },
+
+      timelineWeek: {
+        type              : 'timeline',
+        duration          : { week: 1 },
+        buttonText        : 'week',
+        slotLabelFormat   : 'D',
+        slotDuration      : '24:00:01'
+      }
+    },
+     
+    // Needed due hack partial day events 23:59:59
+    dayRender: function (date, cell) {
+      if ($('#calendar').fullCalendar('getView').name == 'timelineYear') {
+        cell.removeClass('fc-sat'); 
+        cell.removeClass('fc-sun'); 
+        return;
+      }; 
+
+      if (date.day() == 0) {
+        cell.addClass('fc-sun'); };
+                        
+      if (date.day() == 6) {
+        cell.addClass('fc-sat'); };
+
+      if (date.format('YYYY-MM-DD') == moment().format('YYYY-MM-DD')) {
+          cell.addClass ('fc-today'); };
+    },
+    
+    resourceRender: function (resourceObj, labelTds, bodyTds) {
+      labelTds.find('span.fc-cell-text').html (
+      '<b><a href="deviceDetails.php?mac='+ resourceObj.id+ '" class="">'+ resourceObj.title +'</a></b>');
+
+      // Resize heihgt
+      // $(".fc-content table tbody tr .fc-widget-content div").addClass('fc-resized-row');
+    },
+ 
+    eventRender: function (event, element, view) {
+      $(element).tooltip({container: 'body', placement: 'right', title: event.tooltip});
+      // element.attr ('title', event.tooltip);  // Alternative tooltip
+    },
+
+    loading: function( isLoading, view ) {
+        if (isLoading) {
+          $("#loading").show();
+        } else {
+          $("#loading").hide();
+        }
+    }
+
+  })
+}
+
+
+// -----------------------------------------------------------------------------
+function getDevicesTotals () {
+  // stop timer
+  stopTimerRefreshData();
+
+  // get totals and put in boxes
+  $.get('php/server/devices.php?action=getDevicesTotals', function(data) {
+    var totalsDevices = JSON.parse(data);
+
+    $('#devicesAll').html        (totalsDevices[0].toLocaleString());
+    $('#devicesConnected').html  (totalsDevices[1].toLocaleString());
+    $('#devicesFavorites').html  (totalsDevices[2].toLocaleString());
+    $('#devicesNew').html        (totalsDevices[3].toLocaleString());
+    $('#devicesDown').html       (totalsDevices[4].toLocaleString());
+    $('#devicesHidden').html     (totalsDevices[5].toLocaleString());
+
+    // Timer for refresh data
+    newTimerRefreshData (getDevicesTotals);
+  } );
+}
+
+
+// -----------------------------------------------------------------------------
+function getDevicesPresence (status) {
+  // Save status selected
+  deviceStatus = status;
+
+  // Defini color & title for the status selected
+  switch (deviceStatus) {
+    case 'all':        tableTitle = 'All Devices';        color = 'aqua';    break;
+    case 'connected':  tableTitle = 'Connected Devices';  color = 'green';   break;
+    case 'favorites':  tableTitle = 'Favorites';          color = 'yellow';  break;
+    case 'new':        tableTitle = 'New Devices';        color = 'yellow';  break;
+    case 'down':       tableTitle = 'Down Alerts';        color = 'red';     break;
+    case 'archived':   tableTitle = 'Archived Devices';   color = 'gray';    break;
+    default:           tableTitle = 'Devices';            color = 'gray';    break;
+  } 
+
+  // Set title and color
+  $('#tableDevicesTitle')[0].className = 'box-title text-'+ color;
+  $('#tableDevicesBox')[0].className = 'box box-'+ color;
+  $('#tableDevicesTitle').html (tableTitle);
+
+  // Define new datasource URL and reload
+  $('#calendar').fullCalendar ('option', 'resources', 'php/server/devices.php?action=getDevicesListCalendar&status='+ deviceStatus);
+  $('#calendar').fullCalendar ('refetchResources');
+
+  $('#calendar').fullCalendar('removeEventSources');
+  $('#calendar').fullCalendar('addEventSource', { url: 'php/server/events.php?action=getEventsCalendar' });
+};
+
+</script>
